acdc4e722f6a5003a22adcf6160afbfc91a488b9
Aryansh1412 <aryanshjain1412@gmail.com>
2024-07-15 21:58:47 +0530

    vercel.json added

--- Server.js
+++ Server.js
@@ -58,5 +58,5 @@ socket.on(ACTIONS.SYNC_CODE, ({ socketId, code }) => {
   });
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
