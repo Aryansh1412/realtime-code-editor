2637f0df341c615153b48a11f58806eba5d59369
Aryansh1412 <aryanshjain1412@gmail.com>
2024-07-15 22:28:44 +0530

    codemirror error

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useRef } from "react";
-import Codemirror from "codemirror-5.65.16";
-import "codemirror-5.65.16/lib/codemirror.css";
-import "codemirror-5.65.16/theme/dracula.css";
-import "codemirror-5.65.16/mode/javascript/javascript";
-import "codemirror-5.65.16/addon/edit/closetag";
-import "codemirror-5.65.16/addon/edit/closebrackets";
+import Codemirror from "codemirror";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/dracula.css";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/addon/edit/closetag";
+import "codemirror/addon/edit/closebrackets";
 import ACTIONS from "../Actions";
 
 const Editor = ({ socketRef, roomId, onCodeChange }) => {
